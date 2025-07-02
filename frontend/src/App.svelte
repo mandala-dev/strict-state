5e8398bfcb51197b3a1338a79ee4c15b90dd2462
LemiBijafra <grudla6502@gmail.com>
2025-07-02 08:39:28 +0200

    God rid of some temp/POC stuff.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,91 +1,16 @@
 <script>
-  import { Canvas } from "@threlte/core";
-  import Scene from "./Scene.svelte";
-  import AnimTimeline from "./AnimTimeline.svelte"
-  import Timeline from "./Timeline.svelte"
-  import Scene1 from "./Scene1.svelte";
-
-  import Prism from "prismjs";
-  import CodeInput from "@srsholmes/svelte-code-input/CodeInput.svelte";
-
-  import { AceEditor } from "svelte-ace";
-  import "brace/mode/json";
-  import "brace/theme/chrome";
-  import TreeView from "./TreeView.svelte";
   let text = "";
   import JSONTree from "./lib/svelte-json-tree/index";
   import CodeMirror from "svelte-codemirror-editor";
   import { javascript } from "@codemirror/lang-javascript";
-  import Draggable from './Draggable.svelte';
   import { oneDark } from "@codemirror/theme-one-dark";
-  import StateMachineEditor from "./StateMachineEditor.svelte";
-  import Nodes1 from "./Nodes1.svelte";
   import Statechart from "./Statechart.svelte";
 
-  let redrawn = 0;
-  let redraw = () => {
-    ++redrawn;
-  }; // <= you can bind to this, too
-
-  // const handleWheel = e => {
-  // console.log(e);
-  // e.preventDefault();
-  //
-  import { shader, toggleRedraw } from "./lib/shared.svelte.js";
+  import { sm, smSrc, shader } from "./lib/shared.svelte.js";
   import { Pane, Splitpanes } from "svelte-splitpanes";
 
-  //  setInterval(() => {
-  //    console.log(redrawn)
-  //  }, 5000)
-  const value = {
-    array: [1, 2, 3],
-    bool: true,
-    object: {
-      foo: "bar",
-    },
-  };
-  let tree_data = [
-    {
-      name: "Group",
-      selected: true,
-      children: [
-        {
-          name: "Sub Group",
-          children: [
-            {
-              name: "Item 1.1.1",
-            },
-            {
-              name: "Item 1.1.2",
-            },
-          ],
-        },
-        {
-          name: "Group",
-        },
-      ],
-    },
-    {
-      name: "Item 2",
-      children: [
-        {
-          name: "Item 2.1",
-        },
-        {
-          name: "Item 2.2",
-        },
-      ],
-    },
-    {
-      name: "Item 3",
-    },
-  ];
+  const ast = sm;
 </script>
-
-<!-- <svelte:window on:wheel|preventDefault={handleWheel} /> -->
-<!-- <CodeMirror bind:value lang={javascript()} theme={oneDark} /> -->
-
-<!-- <TreeView tree_data={document.getRootNode()} /> -->
 
 <Splitpanes horizontal={true} style="height: 100vh">
   <Pane>
@@ -94,79 +19,18 @@
         1
         <br />
         <em class="specs">I have a min width of 20%</em>
-         <button on:click={() => {toggleRedraw()}}>Apply</button>
-        <CodeMirror bind:value={shader.fragment} lang={javascript()} />
+        <button on:click={() => {}}>Apply</button>
+        <CodeMirror bind:value={smSrc.text} lang={javascript()} />
       </Pane>
-      <Pane minSize={20}>
-        1
-        <br />
-        <em class="specs">I have a min width of 20%</em>
-         <button on:click={() => {toggleRedraw()}}>Apply</button>
-        <CodeMirror bind:value={shader.vertex} lang={javascript()} />
+      <Pane>
+        <JSONTree value={ast} />
       </Pane>
-      <Pane minSize={15}>
-        2
-        <br />
-        <em class="specs">I have a min height of 15%</em>
-        <Canvas>
-          <Scene />
-        </Canvas>
-      </Pane>
-        <Pane>
-          <JSONTree {value} />
-          5</Pane>
     </Splitpanes>
   </Pane>
   <Pane>
-    <!-- <Canvas>
-      <Scene1 bind:redraw={redrawn} />
-    </Canvas> -->
-    <!-- <StateMachineEditor/> -->
-     <Nodes1/>
-      <!-- <Statechart/> -->
-    </Pane>
-    <Pane><Timeline/></Pane>
-    <Pane>
-      <AnimTimeline
-        time={0}
-  model={{
-    rows: [
-      {
-        keyframes: [
-          {
-            val: 40,
-          },
-          {
-            val: 3000,
-          },
-        ],
-      },
-    ],
-  }}>
-  </AnimTimeline>
-    </Pane>
+    <Statechart />
+  </Pane>
 </Splitpanes>
-
-<!-- <TreeView tree_data = {tree_data} /> -->
-
-<!-- <AceEditor
-  on:selectionChange={(obj) => console.log(obj.detail)}
-  on:paste={(obj) => console.log(obj.detail)}
-  on:input={(obj) => console.log(obj.detail)}
-  on:focus={() => console.log('focus')}
-  on:documentChange={(obj) => console.log(`document change : ${obj.detail}`)}
-  on:cut={() => console.log('cut')}
-  on:cursorChange={() => console.log('cursor change')}
-  on:copy={() => console.log('copy')}
-  on:init={(editor) => console.log(editor.detail)}
-  on:commandKey={(obj) => console.log(obj.detail)}
-  on:changeMode={(obj) => console.log(`change mode : ${obj.detail}`)}
-  on:blur={() => console.log('blur')}
-  width='100%'
-  height='300px'
-  lang="glsl"
-  theme="twilight"
-  value={text} /> -->
 
 <style>
   :global(body, html) {
--- frontend/src/lib/shared.svelte.js
+++ frontend/src/lib/shared.svelte.js
@@ -1,5 +1,6 @@
 import fragmentShader from '../fragment.glsl?raw'
 import vertexShader from '../vertex.glsl?raw'
+import smcat from "../../../state-machine-cat/src/parse/index.mts";
 
 export let shader = $state({
 	vertex: vertexShader,
@@ -7,22 +8,85 @@ export let shader = $state({
   redraw: false
 })
 
-export let toggleRedraw = () => {
-  shader.redraw = !shader.redraw
-}
+export let smSrc = $state({text : `
+a :    entry/ write unit test
+       write code
+       exit/ ...,
+b {
+    # ba is really part of b's statemachine
+    # and not of the outer statemachine
+    ba, bb {
+        bba, bbb;
+        bba => bbb;
+        bbb => bba;
+    };
+    ba => bb: one;
+    ba => bc: two;
+},
+c {
+    ca {
+      caa {
+        caaa;
+        caaa => final;
+      };
+    };
+},
+final;
 
-// export let shader =
-//   class {
-//     vertex = $state()
-//     fragment = $state()
-//     redraw = $state(false)
-//     toggleRedraw() {
-//       this.redraw = !this.redraw
-//     }
-//     constructor() {
-//       this.vertex = vertexShader
-//       this.fragment = fragmentShader
-//     }
-//   }
 
+a => caaa : a => caaa;
+initial => a;
+a => ba;
 
+/* transition to composite ... */
+/* ... bare */
+a => b;
+
+/* ... with a note */
+# note for a => b
+a => b;
+
+/* ... with a label */
+a => b: a => b;
+
+/* ... with a note and a label */
+# note for caa => final: caa => final
+a => b: a => b;
+
+/* transition from composite ... */
+/* ... bare */
+caa => final;
+
+/* ... with a note */
+# note for caa => final
+caa => final;
+
+/* ... with a label */
+caa => final: caa => final;
+
+/* ... with a note and a label */
+# note for caa => final: caa => final
+caa => final: caa => final;
+`})
+
+const ast = smcat.getAST(smSrc.text, null)
+
+export let sm = $state({
+  addParents: function () {
+    for (var i in this) {
+      if (typeof this[i] == "object") {
+        this[i].addParents = this.addParents;
+        this[i].addParents();
+        this[i].parent = this;
+      }
+    }
+    return this;
+  },
+  ...ast
+})
+  
+sm.addParents()
+
+// export let toggleRedraw = () => {
+//   shader.redraw = !shader.redraw
+// }
