9edbcc11619bd4ba1e4924488ec4756016086fa2
LemiBijafra <grudla6502@gmail.com>
2025-07-10 22:43:13 +0200

    Missing ref added.

---
+++ src/Statechart_Initial_State.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+
+  let { isConnectable }: NodeProps = $props();
+</script>
+
+<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="10" cy="10" r="10" />
+</svg>
+
+<div></div>
+<Handle type="source" position={Position.Right} {isConnectable} />
